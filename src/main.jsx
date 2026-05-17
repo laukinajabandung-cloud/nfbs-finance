ced05ebf81d9151d8670e11b8de148aad9cf28f2
Abu Nazra <laukinaja.bandung@gmail.com>
2026-05-17 13:46:32 +0700

    fix mutlak menyelaraskan struktur html dan main jsx standar vite

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // Pastikan baris CSS ini disesuaikan dengan file asli Umi jika berbeda
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
