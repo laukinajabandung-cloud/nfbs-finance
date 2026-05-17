f8d428928248a724ecfe40f86f4ee3b478e3750d
Abu Nazra <laukinaja.bandung@gmail.com>
2026-05-17 13:35:51 +0700

    fix menormalkan main jsx dan index html ke standar murni vite

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // Pastikan baris ini tetap ada jika Umi pakai Tailwind CSS
+import './index.css' // Pastikan baris CSS ini disesuaikan dengan file asli Umi jika berbeda
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
